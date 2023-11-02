12b136d77ddc6e40133984bbf18b54e6129b37dc
Kansiime <nevercekansiime@gmail.com>
2023-11-01 22:32:24 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,9 @@ for i in range(0, 100):
     if i != 99:
         print("{:02d}, ".format(i), end="")
     else:
+        print("{:02d}".format(i))#!/usr/bin/python3
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
         print("{:02d}".format(i))
-
