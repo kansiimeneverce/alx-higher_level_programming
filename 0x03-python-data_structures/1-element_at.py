26061229b4ddedd36209feb27b661416381a69a4
Kansiime <nevercekansiime@gmail.com>
2023-11-05 15:13:09 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
+def element_at(my_list, idx):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
 
-def print_list_integer(my_list=[]):
-    """
-    prints a list of given integers
-    """
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    return (my_list[idx])
