fda7fe4e7b7cbe5ebbc482e2b5ae8005cf16258a
Kansiime <nevercekansiime@gmail.com>
2023-11-20 10:10:12 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    printed = 0
+    count = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            printed += 1
-        except:
-            continue
+        except Exception as e:
+            break
+        else:
+            count += 1
     print()
-    return printed
+    return count
