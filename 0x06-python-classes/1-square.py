a79ef31511a32ffeedc07fca812797754040741f
Kansiime <nevercekansiime@gmail.com>
2023-11-23 05:40:21 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a Class Square"""
+
+
+class Square:
+    """
+    Class that defines properties of square by: (based on 0-squre.py)
+
+     Attributes:
+        size: size of a square (1 side).
+    """
+    def __init__(self, size):
+        """Creates new instances of square (1 side).
+
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
+
+
+
